19d3332c02cee5dd8d49a27ee81bdd1347840ab4
Rob Tillaart <rob.tillaart@gmail.com>
2023-10-31 10:57:05 +0100

    update readme.md (#17)
    - update readme.md (badges).
    - add performance test example (another).
      - add UNO + ESP32 test results.
    - minor edits.

--- examples/fastMap_performance_test/fastMap_performance_test.ino
+++ examples/fastMap_performance_test/fastMap_performance_test.ino
@@ -1,5 +1,5 @@
 //
-//    FILE: fastMapDemo.ino
+//    FILE: fastMap_performance_test.ino
 //  AUTHOR: Rob Tillaart
 // PURPOSE: demo of FastMap class ==> a faster map function
 //    DATE: 2014-11-02
@@ -14,19 +14,25 @@ uint32_t reference;
 
 volatile long zz = 3000, yy = 20000;
 volatile float x;
+volatile int z;
 
 FastMap mapper;
+FastMapDouble mapperD;
 
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("Start fastMapDemo\nlib version: ");
+  Serial.println();
+  Serial.println(__FILE__);
+  Serial.print("FASTMAP_LIB_VERSION: ");
   Serial.println(FASTMAP_LIB_VERSION);
   Serial.println();
+  delay(100);
 
   //  Get a non optimizable value;
-  int z = analogRead(A0);
+  z = analogRead(A0);
+
 
   //  REFERENCE
   start = micros();
@@ -35,12 +41,16 @@ void setup()
     x = map(z, 0, 1023, yy, zz);
   }
   stop = micros();
-  reference = stop-start;
+  reference = stop - start;
+  Serial.print("MAP:\t");
   Serial.println(reference);
   Serial.print(z);
   Serial.print(" -> ");
   Serial.println(x);
   Serial.println();
+  delay(100);
+
+
 
   //  FASTMAP
   mapper.init(0, 1023, yy, zz);
@@ -50,14 +60,35 @@ void setup()
     x = mapper.map(z);
   }
   stop = micros();
-  Serial.println(stop-start);
+  Serial.print("FASTMAP:\t");
+  Serial.println(stop - start);
   Serial.print(z);
   Serial.print(" -> ");
   Serial.println(x);
-
-  //  GAIN
   Serial.print("Performance factor: ");
-  Serial.println(reference/(stop-start));
+  Serial.println(1.0 * reference / (stop - start));
+  Serial.println();
+  delay(100);
+
+
+
+  //  FASTMAPDOUBLE
+  mapperD.init(0, 1023, yy, zz);
+  start = micros();
+  for (int i = 0; i < 10000; i++)
+  {
+    x = mapperD.map(z);
+  }
+  stop = micros();
+  Serial.print("FASTMAPD:\t");
+  Serial.println(stop - start);
+  Serial.print(z);
+  Serial.print(" -> ");
+  Serial.println(x);
+  Serial.print("Performance factor: ");
+  Serial.println(1.0 * reference / (stop - start));
+  Serial.println();
+
 }
 
 
@@ -67,4 +98,3 @@ void loop()
 
 
 //  -- END OF FILE --
-
